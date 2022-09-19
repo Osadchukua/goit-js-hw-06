b597c6fdd230abb25f44186f4cc5dd46737bb474
Ivan Osadchuk <osadchukua@gmail.com>
2022-09-19 20:24:15 +0200

    9 good
    10 ?work?

--- js/task-09.js
+++ js/task-09.js
@@ -2,4 +2,11 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const body = document.querySelector('body');
+const bcName = document.querySelector('.color');
+const button = document.querySelector('.change-color');
 
+button.addEventListener('click', () => {
+  body.style.backgroundColor = getRandomHexColor();
+  bcName.textContent = getRandomHexColor();
+});
