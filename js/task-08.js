10624ca881f0838b75908fffeadcec7279bd0dec
Ivan Osadchuk <osadchukua@gmail.com>
2022-09-19 10:14:48 +0200

    8 work
    7 stop
    9 ---
    10 ---

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const { email, password } = evt.currentTarget.elements;
+  const data = {
+    email: email.value,
+    password: password.value,
+    // date: new Date(),
+  };
+  console.log(data);
+  // alert('Thenk you')
+}
