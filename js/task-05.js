0ff80e482a985a17e0d357ecf9a4bee5f00e3e6a
Ivan Osadchuk <osadchukua@gmail.com>
2022-09-23 20:43:56 +0200

    oll good, 10 in work

--- js/task-05.js
+++ js/task-05.js
@@ -1,16 +1,24 @@
 const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
+// const onInputChange = event => {
+
+//   if (event.currentTarget.value.trim() === '') {
+//     output.textContent = 'Anonymous';
+//   } else {output.textContent = null
+
+//     output.textContent = event.currentTarget.value.trim();
+//   }
+// }
+
+// input.addEventListener('input', onInputChange);
+
+// -----------------------------------------------------
 
 const onInputChange = event => {
-  event.currentTarget.value.trim() === '' ? output.textContent = "Anonymous" : null
-  // if (event.currentTarget.value.trim() === '') {
-  //   output.textContent = 'Anonymous';
-  // } else {output.textContent = null
-    
-    output.textContent = event.currentTarget.value.trim();
-  // }
-}
+  event.currentTarget.value === ''
+    ? (output.textContent = 'Anonymous')
+    : output.textContent = event.currentTarget.value.trim();
+};
 
 input.addEventListener('input', onInputChange);
-
--- js/task-10.js
+++ js/task-10.js
@@ -2,15 +2,40 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const input = document.querySelector('input');
+console.dir(input.textContent);
+console.dir(input.value);
+console.log(input);
+
+const create = document.querySelector('button[data-create]');
+// console.dir(create.innerHTML);
+
+const destroy = document.querySelector('button[data-destroy]');
+// console.dir(destroy.innerHTML);
 
 const boxes = document.querySelector('#boxes');
-console.dir(boxes);
+// console.log(boxes);
 
-const input = document.querySelector('input');
-const amount = Number(input.tabIndex);
-console.dir(amount);
+const boxValue = 30;
+// -----------------------------------------------------------
 
-function createBoxes(amount) {
-  const liEl = document.createElement('div');
+const createBoxes = amount => {
+  amount = input.value;
+
+  const divEl = document.createElement('div');
+  divEl.classList.add('box');
+  divEl.style.backgroundColor = getRandomHexColor();
+
+  divEl.style.height = boxValue + 'px';
+  divEl.style.width = boxValue + 'px';
   
-}
+  boxes.append(divEl);
+};
+
+const destroyBoxes = () => {
+  const divBox = document.querySelector('.box');
+  divBox.remove();
+};
+
+create.addEventListener('click', createBoxes);
+destroy.addEventListener('click', destroyBoxes);
